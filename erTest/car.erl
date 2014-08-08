24bfae00859bce03f565e174949b93e8d01927c6
Vikont133 <vikont@frtk.ru>
2014-08-08 17:19:02 +0400

    first commit

---
+++ erTest/car.erl
@@ -0,0 +1,42 @@
+-module(car).
+-export([start/1]).
+
+start(I) ->
+	{_,{route,CarRoute},_} = I, 
+	[CarPosition|_] = CarRoute,
+	Pid = self(),
+	spawn (fun() -> car(Pid, I, CarPosition, CarRoute) end).
+
+distance(Point1, Point2) ->
+	{X1, Y1} = Point1,
+	{X2, Y2} = Point2,
+	math:sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1)).
+
+newposition(Point1, Point2, L) ->
+	{X1, Y1} = Point1,
+	{X2, Y2} = Point2,
+	{X1 + L*(X2-X1)/distance(Point1, Point2), Y1 + L*(Y2-Y1)/distance(Point1, Point2)}.
+
+nextpoint({CarPosition, [], FullRoute, CarSpeed}) ->
+	nextpoint({CarPosition, FullRoute, FullRoute, CarSpeed});
+nextpoint(I) ->
+	{CarPosition, CurrentRoute, FullRoute, CarSpeed} = I,
+	[NextPoint|NewRoute] = CurrentRoute,
+	L = distance(CarPosition, NextPoint),
+	if
+		L =< CarSpeed ->
+			nextpoint({NextPoint, NewRoute, FullRoute, CarSpeed - L});
+		L > CarSpeed ->
+			{newposition(CarPosition, NextPoint, CarSpeed), CurrentRoute}
+	end.
+
+car(Pid, I, CarPosition, CurrentRoute) ->
+	receive
+		after 1000 ->
+			{{number,CarNumber},{route,FullRoute},{speed, CarSpeed}} = I,
+			Info = {CarPosition, CurrentRoute, FullRoute, CarSpeed},
+			{NewPosition, NewRoute} = nextpoint(Info),
+			Pid ! {CarNumber, NewPosition},
+			car(Pid, I, NewPosition, NewRoute) 
+	end.
+
---
+++ erTest/test.erl
@@ -0,0 +1,19 @@
+-module(test).
+-export([main/0]).
+
+createcars([I|T]) ->
+	Pid = car:start(I),
+	[Pid|createcars(T)];
+createcars([]) -> [].
+
+loop() ->
+	receive
+		{CarNumber, CarPosition} ->
+			io:format("Car ~p now at ~p~n", [CarNumber, CarPosition]),
+			loop()
+	end.
+
+main() ->
+	{ok, Default} = file:consult("cars.dat"),
+	createcars(Default),
+	loop().
