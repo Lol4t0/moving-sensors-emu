b6926299f0804aabd215419f7cbbef8c447970a1
Vikont133 <vikont@frtk.ru>
2014-08-12 18:07:52 +0400

    Added records

--- src/erTest_car.erl
+++ src/erTest_car.erl
@@ -1,10 +1,18 @@
 -module(erTest_car).
 -export([start/1]).
+-record(status, {
+		position,
+		number,
+		speed,
+		route,
+		current_route
+	}).
 
 start(I) ->
-	{_,{route,CarRoute},_} = I, 
+	{{number, CarNumber}, {route, CarRoute}, {speed, CarSpeed}} = I, 
 	[CarPosition|_] = CarRoute,
-	{ok, spawn (fun() -> car(I, CarPosition, CarRoute) end)}.
+	CarStatus = #status{position = CarPosition, number = CarNumber, speed = CarSpeed, route = CarRoute, current_route = CarRoute},
+	{ok, spawn (fun() -> car(CarStatus) end)}.
 
 distance(Point1, Point2) ->
 	{X1, Y1} = Point1,
@@ -16,26 +24,25 @@ newposition(Point1, Point2, L) ->
 	{X2, Y2} = Point2,
 	{X1 + L*(X2-X1)/distance(Point1, Point2), Y1 + L*(Y2-Y1)/distance(Point1, Point2)}.
 
-nextpoint({CarPosition, [], FullRoute, CarSpeed}) ->
-	nextpoint({CarPosition, FullRoute, FullRoute, CarSpeed});
-nextpoint(I) ->
-	{CarPosition, CurrentRoute, FullRoute, CarSpeed} = I,
+nextpoint(#status{current_route = []} = CarStatus) ->
+	#status{route = NewRoute} = CarStatus,
+	nextpoint(CarStatus#status{current_route = NewRoute});
+nextpoint(#status{position = CarPosition, current_route = CurrentRoute, speed = CarSpeed} = CarStatus) ->
 	[NextPoint|NewRoute] = CurrentRoute,
 	L = distance(CarPosition, NextPoint),
 	if
 		L =< CarSpeed ->
-			nextpoint({NextPoint, NewRoute, FullRoute, CarSpeed - L});
+			nextpoint(CarStatus#status{position = NextPoint, current_route = NewRoute, speed = CarSpeed - L});
 		L > CarSpeed ->
 			{newposition(CarPosition, NextPoint, CarSpeed), CurrentRoute}
 	end.
 
-car(I, CarPosition, CurrentRoute) ->
+car(CarStatus) ->
 	receive
 		after 1000 ->
-			{{number,CarNumber},{route,FullRoute},{speed, CarSpeed}} = I,
-			Info = {CarPosition, CurrentRoute, FullRoute, CarSpeed},
-			{NewPosition, NewRoute} = nextpoint(Info),
-			io:format("Car ~p now at ~p~n", [CarNumber, CarPosition]),
-			car(I, NewPosition, NewRoute) 
+			#status{number = CarNumber} = CarStatus,
+			{NewPosition, NewRoute} = nextpoint(CarStatus),
+			io:format("Car ~p now at ~p~n", [CarNumber, NewPosition]),
+			car(CarStatus#status{position = NewPosition, current_route = NewRoute}) 
 	end.
 
