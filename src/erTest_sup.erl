29168d3b5ebab0eb5c5f6c3f2bc2ff342ad3f4d5
Vikont133 <vikont@frtk.ru>
2014-08-19 18:49:58 +0400

    Added time measure

--- src/erTest_sup.erl
+++ src/erTest_sup.erl
@@ -8,28 +8,33 @@
 
 -define(SERVER, ?MODULE).
 -define(MAPSIZE, 100).
--define(ROUTES, 3).
--define(CARS, 5).
+-define(ROUTES, 10000).
+-define(CARS, 120000).
 
 start_link() ->
-	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+	statistics(runtime),
+	statistics(wall_clock),
+	supervisor:start_link({local, ?SERVER}, ?MODULE, []),
+	{_, Time1} = statistics(runtime),
+	{_, Time2} = statistics(wall_clock),
+	io:format("Data loaded for = ~p (~p) seconds~n", [Time1 div 1000, Time2 div 1000]).
 
 init([]) ->
 	% {ok, Default} = file:consult(filename:join(code:priv_dir(erTest), "cars.dat")),
 	Default = generate_cars(?CARS, generate_routes(?ROUTES)),
-	print_info(Default),
+	% print_info(Default),
 	{ok,{{one_for_one,5,10}, lists:map(fun view/1, Default)}}.
 
 view(I) ->
 	{{number, CarNumber}, _, _} = I,
 	{CarNumber, {erTest_car, start_link, [I]}, permanent, 2000, worker, [erTest_car]}.
 
-print_info(Cars) ->
-	io:format("Loaded data about cars~n"),
-	io:format("~p~n", [Cars]),
-	io:format("You can use erTest_car:car_start(Number) to start car~n"),
-	io:format("You can use erTest_car:car_stop(Number) to stop car~n"),
-	io:format("You can use erTest_car:car_position(Number) to know current car position~n").
+% print_info(Cars) ->
+% 	io:format("Loaded data about cars~n"),
+% 	io:format("~p~n", [Cars]),
+% 	io:format("You can use erTest_car:car_start(Number) to start car~n"),
+% 	io:format("You can use erTest_car:car_stop(Number) to stop car~n"),
+% 	io:format("You can use erTest_car:car_position(Number) to know current car position~n").
 
 route(0) -> [];
 route(Q) ->
